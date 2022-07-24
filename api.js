03432f893ef7bfd5f6b311b41d7ada6861fd27c2
emanueledb <emanueledibene@gmail.com>
2022-07-24 17:21:15 +0200

    model updated - morgan implemented

--- api.js
+++ api.js
@@ -2,11 +2,13 @@ const express = require("express")
 const Router = require("./modules/users/usersRoutes")
 const mongoose = require("mongoose")
 const helmet = require("helmet")
-const bodyParser = require ("body-parser")
-require('dotenv').config()
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+require("dotenv").config()
 
 const app = express()
 app.use(helmet())
+app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(Router)
@@ -14,8 +16,6 @@ app.use(Router)
 const username = encodeURIComponent(process.env.MONGO_USER)
 const password = encodeURIComponent(process.env.MONGO_PASSWORD)
 const clusterName = encodeURIComponent(process.env.MONGO_URL)
-
-console.log(process.env.MONGO_USER)
 
 let uri = `mongodb+srv://${username}:${password}@${clusterName}.mongodb.net/?retryWrites=true&w=majority`
 
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
 
-const userSchema = new mongoose.Schema(
-  {
-    name: String,
-    pets: Boolean,
-    oranges: Number,
-  },
-)
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  email: { type: String, require: true },
+  age: { type: Number, require: true },
+  hasPets: { type: Boolean, require: true },
+  date: { type: Date, default: Date.now },
+})
 
 const User = mongoose.model("User", userSchema)
 module.exports = User
