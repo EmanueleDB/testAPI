19dd8f8bb407a511d17526ed62bcd4f56503a223
emanueledb <emanueledibene@gmail.com>
2022-07-31 16:06:35 +0200

    cors added

--- api/setup.js
+++ api/setup.js
@@ -1,8 +1,10 @@
 import helmet from 'helmet'
 import morgan from 'morgan'
 import bodyParser from "body-parser";
+import cors from 'cors'
 
 export default (app) => {
+  app.use(cors())
   app.use(helmet())
   app.use(morgan("dev"))
   app.use(bodyParser.urlencoded({ extended: true }))
